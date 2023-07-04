4d237ac4f25ba598aeb3db82b9375419289a2496
Anth0nYM <marquesanthony62@gmail.com>
2023-07-04 15:28:46 -0300

    fazendo o input das tasks

--- app/screens/List.tsx
+++ app/screens/List.tsx
@@ -1,12 +1,53 @@
-import {View, Text} from 'react-native'
-import React from 'react'
+import { View, StyleSheet, TextInput, Button, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+
+export interface Todo {
+	done: boolean;
+	id: string;
+	title: string;
+}
 
 const List = () => {
-    return (
-        <View>
-            <Text>List</Text>
-        </View>
-    );
+	const [todos, setTodos] = useState<any[]>([]);
+	const [todo, setTodo] = useState('');
+
+	const addTodo = async () => {
+		// TODO
+		alert(todo);
+	};
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.form}>
+				<TextInput
+					style={styles.input}
+					placeholder="Add new todo"
+					onChangeText={(text: string) => setTodo(text)}
+					value={todo}
+				/>
+				<Button onPress={addTodo} title="Add Todo" disabled={todo === ''} />
+			</View>
+		</View>
+	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		marginHorizontal: 20
+	},
+	form: {
+		marginVertical: 20,
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
+	input: {
+		flex: 1,
+		height: 40,
+		borderWidth: 1,
+		borderRadius: 4,
+		padding: 10,
+		backgroundColor: '#fff'
+	}
+});
 
 export default List;
--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,19 +1,19 @@
 import { initializeApp } from 'firebase/app';
-import {getAuth} from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyCAdrNdwmjUcJfxZvifV9tofnjQ8d0BLME", 
-    authDomain: "taskmanager-f65a4.firebaseapp.com",
-    databaseURL: "https://taskmanager-f65a4-default-rtdb.firebaseio.com",
-    projectId: "taskmanager-f65a4",
-    storageBucket: "taskmanager-f65a4.appspot.com",
-    messagingSenderId: "234789576581",
-    appId: "1:234789576581:web:d2f5d146ee5f10c1d3eeec",
-    measurementId: "G-PXRJM1640R"
-  };
-  
+  apiKey: "AIzaSyCAdrNdwmjUcJfxZvifV9tofnjQ8d0BLME",
+  authDomain: "taskmanager-f65a4.firebaseapp.com",
+  databaseURL: "https://taskmanager-f65a4-default-rtdb.firebaseio.com",
+  projectId: "taskmanager-f65a4",
+  storageBucket: "taskmanager-f65a4.appspot.com",
+  messagingSenderId: "234789576581",
+  appId: "1:234789576581:web:d2f5d146ee5f10c1d3eeec",
+  measurementId: "G-PXRJM1640R"
+};
+
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
